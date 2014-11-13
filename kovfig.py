3ca0007970056e665b28c62d39ed6073309a97cd
Noriyuki Abe <kenko.py@gmail.com>
2014-11-13 21:14:31 +0900

    Modify path to use __file__

--- kovfig.py
+++ kovfig.py
@@ -1,11 +1,27 @@
 #! /usr/bin/env python
 # coding:utf-8
 
+from os import path
+
 
 # the number of loop for train IBM Model 2
 loop_count = 10
-phrase_model_file = "./phrase.model"
-bigram_model_file = "./bigram.model"
+phrase_model_file = path.join(
+    path.abspath(path.dirname(__file__)),
+    "phrase.model"
+)
+bigram_model_file = path.join(
+    path.abspath(path.dirname(__file__)),
+    "bigram.model"
+)
 
 if __name__ == '__main__':
-    pass
+    print("{} = {}".format(
+        "loop_count",
+        loop_count))
+    print("{} = {}".format(
+        "phrase_model_file",
+        phrase_model_file))
+    print("{} = {}".format(
+        "bigram_model_file",
+        bigram_model_file))
